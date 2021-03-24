e088ef06912c7327a6b94a51141988285bfcc13c
KonstantinK <k.kostromin@bk.ru>
2021-03-24 19:39:29 +0300

    3. Массивы и модели. [#416785]

--- src/main/java/ru/job4j/pojo/Book.java
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -1,4 +1,27 @@
 package ru.job4j.pojo;
 
 public class Book {
+    private String name;
+    private int numberpages;
+
+    public Book(String name, int numberpages){
+        this.name = name;
+        this.numberpages = numberpages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberpages() {
+        return numberpages;
+    }
+
+    public void setNumberpages(int numberpages) {
+        this.numberpages = numberpages;
+    }
 }
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,47 @@
+package ru.job4j.pojo;
+
+/*
+1. Создайте модель данных "Книга" с полями: имя и количество страниц. ru.job4j.pojo.Book.
+
+2. Создайте класс ru.job4j.pojo.Library и в нем метод main.
+
+3. В методе main создайте 4 объекта Book. Добавьте их в массив. Сделайте одну книгу с именем "Clean code".
+
+4. Выведите содержимое массива на консоль через цикл for + index.
+
+5. Переставьте местами книги с индексом 0 и 3. Выведите содержимое массива на консоль.
+
+6. Добавьте цикл с выводом книг с именем "Clean code".
+ */
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Clean code", 100);
+        Book book2 = new Book("Master", 300);
+        Book book3 = new Book("Book3", 10);
+        Book book4 = new Book("Book4", 30);
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+
+        for (int index = 0; index <= 3; index++){
+            Book bk = books[index];
+            System.out.println("In book " + bk.getName() + " number of pages :" + bk.getNumberpages());
+        }
+
+        Book tmp = null;
+        tmp = books[0];
+        books[0] = books[3];
+        books[3] = tmp;
+
+        System.out.println("List of books after change:");
+
+        for (int index = 0; index <= 3; index++){
+            Book bk = books[index];
+            System.out.println("In book " + bk.getName() + " number of pages :" + bk.getNumberpages());
+        }
+    }
+}
