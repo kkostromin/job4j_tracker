fa73da972322970946dc1d1b5dfc7978c8e8d9df
KonstantinK <k.kostromin@bk.ru>
2021-03-28 18:53:57 +0300

    5. Tracker - хранилище [#416793]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,12 +6,24 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-       Item item = new Item();
+       Item item = new Item(0, "1111");
+       Item item2 = new Item(1, "2222");
 
        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
        LocalDateTime itemDate = item.getDate();
 
        String itemDateTimeFormat = itemDate.format(formatter);
        System.out.println("Текущие дата и время после форматирования: " + itemDateTimeFormat);
+
+       Tracker tracker = new Tracker();
+       tracker.add(item);
+       tracker.add(item2);
+       System.out.println("----FindById-----");
+       tracker.findById(1);
+       System.out.println("----FindAll-----");
+       tracker.findAll();
+       System.out.println("----FindByName-----");
+       tracker.findByName("2222");
+
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,18 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
+/*
+
+1. Добавьте недостающие методы в класс Tracker. В классе Tracker должны быть методы:
+добавление заявок - public Item add(Item item);
+получение списка всех заявок - public Item[] findAll();
+получение списка по имени - public Item[] findByName(String key);
+получение заявки по id - public Item findById(int id);
+2. Создайте класс ru.job4j.tracker.StartUI. В нем создайте метод main.
+    Внутри метода main создайте объект Tracker. Вызовите у него метод add с объектом Item.
+    Вызовите метод findById и найденный Item выведите на консоль.
+ */
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,12 +24,54 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll(){
+        int size = 0;
+        Item[] itemWithoutNull = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemWithoutNull[size] = item;
+                size++;
+            }
+        }
+        itemWithoutNull = Arrays.copyOf(itemWithoutNull, size);
+        for (int index = 0; index < itemWithoutNull.length; index++) {
+            System.out.println("Запись с id : " + index + " --- " + itemWithoutNull[index].getName());
+        }
+        return itemWithoutNull;
+    }
+
+    public Item[] findByName(String key){
+        int size = 0;
+        String str = "";
+        Item[] itemResults = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            try {
+                str = item.getName();
+                if (str.equals(key)) {
+                    itemResults[size] = item;
+                    size++;
+                }
+            }
+            catch (NullPointerException e){
+                //System.out.println(" item is null ");
+            }
+        }
+        itemResults = Arrays.copyOf(itemResults, size);
+        for (int index = 0; index < size; index++) {
+            System.out.println("Запись : " + " --- " + itemResults[index].getName());
+        }
+        return itemResults;
+    }
+
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
                 rsl = item;
+                System.out.println("Запись с id : " + id + " --- " + item.getName());
                 break;
             }
         }
