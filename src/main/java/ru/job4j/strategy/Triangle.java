d680bf7ee11c1fb49df8aae8c2fd994a90e4944e
KonstantinK <k.kostromin@bk.ru>
2021-05-03 16:06:12 +0300

    7. Шаблон проектирования - Стратегия [#416727]

--- src/main/java/ru/job4j/strategy/Triangle.java
+++ src/main/java/ru/job4j/strategy/Triangle.java
@@ -5,12 +5,8 @@ public class Triangle implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return    "   ^    " + ln
-                + " /   \\ " + ln
-                + "/_____\\" + ln + ln;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Triangle().draw());
+        return "   ^    " + ln
+             + " /   \\ " + ln
+             + "/_____\\" + ln + ln;
     }
 }
