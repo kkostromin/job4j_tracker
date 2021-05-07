3c8ccad17c1e18e90cedadcbee0c0c448edfccb8
KonstantinK <k.kostromin@bk.ru>
2021-05-07 17:32:17 +0300

    0.3. Тестирование исключений с junit [#416734]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,15 +2,11 @@ package ru.job4j.ex;
 
 public class Fact {
 
-    public static void main(String[] args) {
-        System.out.println("Fact 5 : " + calc(-5));
-    }
-
-    public static int calc(int n) {
-        int rsl = 1;
+     public int calc(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Error in argument calc() : n < 0");
         }
+        int rsl = 1;
         for (int index = 1; index <= n; index++) {
             rsl *= index;
         }
