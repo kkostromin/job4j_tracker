2302796b1c2d899fe9ff5a296e450c6dd0039f73
KonstantinK <k.kostromin@bk.ru>
2021-06-21 16:41:02 +0300

    4. final [#334073 #169651]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int tempCountEngine = this.getName().equals("A380") ? 4 : COUNT_ENGINE;
+        int tempCountEngine = name.equals("A380") ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + tempCountEngine);
     }
 
