3ceb030ce4a23c9eb5dfc20cd3448c0ffe40af24
KonstantinK <k.kostromin@bk.ru>
2021-07-09 17:47:40 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #175166]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -1,15 +1,16 @@
 package ru.job4j.collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> coll = new ArrayList<String>();
-        coll.add("Petr");
-        coll.add("Ivan");
-        coll.add("Stepan");
-        for (Object value : coll) {
-            System.out.println(value);
+        ArrayList<String> colls = new ArrayList<>();
+        colls.add("Petr");
+        colls.add("Ivan");
+        colls.add("Stepan");
+        for (String coll : colls) {
+            System.out.println(coll);
         }
     }
 }
